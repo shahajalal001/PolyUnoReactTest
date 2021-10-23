2e99854a7b9fdf66fa5adbbc0ec5dea0facfaeb3
shahajalal001 <shahajalal001@gmail.com>
2021-10-23 22:47:12 +0600

    design improvement

--- components/modal/index.js
+++ components/modal/index.js
@@ -113,9 +113,9 @@ const Modal = ({setShowModal, setQuestion}) => {
     return(
         <>
             <div
-                className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+                className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-gray-900 bg-opacity-25"
             >
-                <div className="relative w-auto my-6 mx-auto max-w-3xl">
+                <div className="relative my-6 mx-auto max-w-3xl shadow-2xl" style={{width: 450}}>
                     {/*content*/}
                     <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                         {/*header*/}
