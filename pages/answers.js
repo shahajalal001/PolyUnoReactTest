740e3f500ce7287c5de6cda49721b720fd1cf670
shahajalal001 <shahajalal001@gmail.com>
2021-10-24 19:55:41 +0600

    answer view added

--- pages/answers.js
+++ pages/answers.js
@@ -21,41 +21,48 @@ const Answers = () => {
             }
         })
     })
+    const handleModal = async (answers) => {
+        console.log(answers)
+        await router.push({pathname: '/answerview', query: {data: JSON.stringify(answers)}})
+    }
     return (
-        <div className="bg-white rounded-lg shadow-lg py-6">
-            <div className="block overflow-x-auto mx-6">
-                <table className="w-full text-left rounded-lg">
-                    <thead>
-                    <tr className="text-gray-800 border border-b-0">
-                        <th className="px-4 py-3">#</th>
-                        <th className="px-4 py-3">Answer Name</th>
-                        <th className="px-4 py-3">ID</th>
-                        <th className="px-4 py-3">Action</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {
-                        questions.map((question, index) => {
-                            return(
-                                <tr className="w-full font-light text-gray-700 bg-gray-100 whitespace-no-wrap border" key={`table-question-${index}`}>
-                                    <td className="px-4 py-4">{++index}</td>
-                                    <td className="px-4 py-4">{`Answer ${index}`}</td>
-                                    <td className="px-4 py-4">{question._id}</td>
-                                    <td className="px-4 py-4">
-                                        <button >
-                                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
-                                            </svg>
-                                        </button>
-                                    </td>
-                                </tr>
-                            )
-                        })
-                    }
-                    </tbody>
-                </table>
+        <div>
+            <div className="bg-white rounded-lg shadow-lg py-6">
+                <div className="block overflow-x-auto mx-6">
+                    <table className="w-full text-left rounded-lg">
+                        <thead>
+                        <tr className="text-gray-800 border border-b-0">
+                            <th className="px-4 py-3">#</th>
+                            <th className="px-4 py-3">Answer Name</th>
+                            <th className="px-4 py-3">ID</th>
+                            <th className="px-4 py-3">Action</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {
+                            questions.map((question, index) => {
+                                return(
+                                    <tr className="w-full font-light text-gray-700 bg-gray-100 whitespace-no-wrap border" key={`table-question-${index}`}>
+                                        <td className="px-4 py-4">{++index}</td>
+                                        <td className="px-4 py-4">{`Answer ${index}`}</td>
+                                        <td className="px-4 py-4">{question._id}</td>
+                                        <td className="px-4 py-4">
+                                            <button onClick={() => handleModal(question.question)}>
+                                                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+                                                </svg>
+                                            </button>
+                                        </td>
+                                    </tr>
+                                )
+                            })
+                        }
+                        </tbody>
+                    </table>
+                </div>
             </div>
+
         </div>
     )
 }
